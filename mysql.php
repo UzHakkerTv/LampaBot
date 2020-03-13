a4693d3df9d1de9d522af09f401e04c29f0caf5f
UzHakkerTv <62137063+UzHakkerTv@users.noreply.github.com>
2020-03-13 15:44:27 +0500

    Create mysql.php

---
+++ mysql.php
@@ -0,0 +1,22 @@
+<?php
+
+$bot_token= '668162302:AAF_uXhNrWm-ZrvUbxM-55T7ULsjvVcmcKY'; //bot tokeni
+$bot_name = 'MegaCoinBot'; //botingiz userini @ belgisini quymasdan kiriting
+
+
+$azobulish = '0.30';
+$azotuplash = '0.50';
+$reftuplash = '0.30';
+$minimalka = '5';
+
+$admin = '572730244'; //Admin ID raqamini kiriting 
+$admin2 = '434787556';
+
+
+$dbhost='localhost'; //bunga tegmang
+$dbname='bot'; //mysql nomi
+$dbuser='bot'; //mysql nomi bunga ham
+$dbpass='bot'; //mysql paroli
+
+
+?>
